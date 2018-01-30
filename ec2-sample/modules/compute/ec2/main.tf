53ada1b4fcba99df85e064fcd6da394e9e598245
ono-hiroshi <hiroshi_ono_1989@yahoo.co.jp>
2018-01-30 23:03:50 +0900

    fix 変数化

--- ec2-sample/modules/compute/ec2/main.tf
+++ ec2-sample/modules/compute/ec2/main.tf
@@ -32,7 +32,7 @@ data "aws_ami" "amazon_linux" {
 
 resource "aws_instance" "web-server" {
   ami                    = "${data.aws_ami.amazon_linux.id}"
-  instance_type          = "t2.micro"
+  instance_type          = "${var.instance_type}"
   key_name               = "${var.key_name}"
   subnet_id              = "${var.subnet_id}"
   vpc_security_group_ids = ["${var.security_group_id}"]
